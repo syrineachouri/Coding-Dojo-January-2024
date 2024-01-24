4208dd4ee835c5cc38ef088eb03d9b3d0693d3f2
syrineachouri <syrinemedo0@gmail.com>
2024-01-24 16:59:04 +0100

    app

---
+++ Programming Basics/stretch_feature.js
@@ -0,0 +1,24 @@
+//stretch feature 1 
+
+var height = 45
+var age = 12
+if (age < 10) {
+    console.log( "Sorry kiddo. Maybe next year.");
+} else if(height >=42){
+    console.log("Get on that ride, kiddo!")
+}else console.log("Sorry kiddo. Maybe next year.");
+
+
+// stretch feature 2
+var height = 45
+var age = 11
+if (age >= 10) {
+    console.log("Get on that ride, kiddo!");
+}
+    else if(height >= 42){
+        console.log("Get on that ride, kiddo!");
+    }
+    
+else { console.log("Sorry kiddo. Maybe next year.");
+    
+}
