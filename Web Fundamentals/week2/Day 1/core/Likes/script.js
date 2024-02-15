5b34decfbf66a4bd520994a2c40a8968260db46d
syrineachouri <syrinemedo0@gmail.com>
2024-02-16 00:19:11 +0100

    Likes.

---
+++ Web Fundamentals/week2/Day 1/core/Likes/script.js
@@ -0,0 +1,21 @@
+var count = 6;
+var increase_Likes = document.querySelector(".increase");
+function increaseLike(){
+    count++ ;
+    increase_Likes.innerText= count + " like(s)";
+    
+}
+var x=12;
+var caca = document.querySelector(".increase1");
+function increaseLike1(){
+    x++ ;
+    caca.innerText= x + " like(s)";
+
+}
+var y=9;
+var coco = document.querySelector(".increase2");
+function increaseLike2(){
+    y++ ;
+    coco.innerText= y + " like(s)";
+
+}
