9c80c6e1125a81f65d8078bba62f96a4308a5b5e
syrineachouri <157491124+syrineachouri@users.noreply.github.com>
2024-04-12 14:23:12 +0100

    mern.

---
+++ MERN/Week 1/Day 3/Core/First_JSX/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Hello Dojo</h1>
+      <h5>Things i need to do</h5>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dog</li>
+      </ul>
+    </div>
+  
+  );
+}
+
+export default App;
