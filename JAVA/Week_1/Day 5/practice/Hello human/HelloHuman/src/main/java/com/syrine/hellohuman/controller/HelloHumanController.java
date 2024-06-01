45c88b122f92e8f6d37e07e455aa424acf8dbdc2
syrineachouri <157491124+syrineachouri@users.noreply.github.com>
2024-06-01 16:39:48 +0100

    Java

---
+++ JAVA/Week_1/Day 5/practice/Hello human/HelloHuman/src/main/java/com/syrine/hellohuman/controller/HelloHumanController.java
@@ -0,0 +1,16 @@
+package com.syrine.hellohuman.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloHumanController {
+	@RequestMapping("/")
+	public String index(@RequestParam(value=("name"), required=false)String searchQuery) {
+		if (searchQuery == null) {
+			return "Hello Human!";
+		} else {
+			return "Hello " + searchQuery ;
+		}
+	}
+}
