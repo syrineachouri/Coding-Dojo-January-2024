532118bc5daaae30a5667ea105cbbe693e05895d
syrineachouri <157491124+syrineachouri@users.noreply.github.com>
2024-06-04 13:23:29 +0100

    Java

---
+++ JAVA/week_2/day_1/practice/Books_API/bookapi/src/main/java/com/syrine/bookapi/repositories/BookRepository.java
@@ -0,0 +1,17 @@
+package com.syrine.bookapi.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.syrine.bookapi.models.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+List<Book> findAll();
+//this method finds books with descriptions containing the search string
+List<Book> findByDescriptionContaining(String search);
+// this method counts how many titles contain a certain string
+Long countByTitleContaining(String search);
+// this method deletes a book that starts with a specific title
+Long deleteByTitleStartingWith(String search);
+}
